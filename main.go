ffd5e992b8601758c78cc7bc392827239c72bd31
Victor-Ihemadu <victorihemadu@gmail.com>
2022-03-30 09:13:57 +0100

    working software

--- main.go
+++ main.go
@@ -1,53 +1,137 @@
 package main
 
 import (
-	"fmt"
 	"github.com/go-chi/chi"
+	"github.com/google/uuid"
+	"html/template"
 	"log"
 	"net/http"
 )
+
+func handleErr(err error) {
+	if err != nil {
+		log.Println(err)
+	}
+}
 
 type blog struct {
 	Id      string
 	Topic   string
 	Content string
-}
-type DataStructure struct {
-	DataStructure []blog
+	State   bool
+	Edit    string
+	Delete  string
 }
 
-var Data1 = DataStructure{DataStructure: []blog{}}
+var DataStructure []blog
 
-var content blog
+var v blog
 
 func main() {
 	r := chi.NewRouter()
 	register(r)
 
-	err := http.ListenAndServe(":8082", r)
-	log.Println("running app on port :8082")
-	if err != nil {
-		fmt.Println(err)
+	err := http.ListenAndServe(":8080", r)
+	log.Println("running app on port :8080")
+	handleErr(err)
+
+}
+
+func register(router *chi.Mux) {
+	router.Get("/", index)
+	router.Get("/newpost", getContent)
+	router.Get("/delete/{Id}", delete)
+	router.Post("/newpost", postContent)
+	router.Get("/update/{Id}", update)
+	router.Post("/update/{Id}", postUpdate)
+}
+
+func index(w http.ResponseWriter, r *http.Request) {
+	s, err := template.ParseFiles("template/index.html")
+	handleErr(err)
+
+	err = s.Execute(w, DataStructure)
+	handleErr(err)
+
+}
+
+func getContent(w http.ResponseWriter, r *http.Request) {
+	s, err := template.ParseFiles("template/newpost.html")
+	handleErr(err)
+
+	err = s.ExecuteTemplate(w, "newpost.html", nil)
+	handleErr(err)
+}
+
+func postContent(w http.ResponseWriter, r *http.Request) {
+	p := blog{}
+
+	err := r.ParseForm()
+	handleErr(err)
+
+	topic := r.PostForm.Get("topic")
+	content := r.PostForm.Get("content")
+
+	p.Id = uuid.NewString()
+	p.Topic = topic
+	p.Content = content
+	p.State = true
+
+	DataStructure = append(DataStructure, p)
+
+	http.Redirect(w, r, "/", 302)
+}
+
+func delete(w http.ResponseWriter, r *http.Request) {
+	id := chi.URLParam(r, "Id")
+
+	for i, _ := range DataStructure {
+		if id == DataStructure[i].Id {
+
+			//Deletes the item with such index
+			DataStructure = append(DataStructure[:i], DataStructure[i+1:]...)
+		}
 	}
 
+	http.Redirect(w, r, "/", 302)
 }
 
-func register(r *chi.Mux) {
-	r.Get("/", homePage)
-	r.Get("/create", newPost)
-	r.Get("/delete/{Id}", deletePost)
-	r.Get("/edit", editPost)
+func update(w http.ResponseWriter, r *http.Request) {
+	id := chi.URLParam(r, "Id")
+
+	for i, _ := range DataStructure {
+		if id == DataStructure[i].Id {
+
+			v = DataStructure[i]
+
+			s, err := template.ParseFiles("template/editpost.html")
+			handleErr(err)
+
+			err = s.Execute(w, DataStructure[i])
+			handleErr(err)
+
+		}
+	}
 }
 
-//func ArticleCtx(next http.Handler) http.Handler {
-//	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-//		articleID := chi.URLParam(r, "articleID")
-//		article, err := dbGetArticle(articleID)
-//		if err != nil {
-//			http.Error(w, http.StatusText(404), 404)
-//			return
-//		}
-//		ctx := context.WithValue(r.Context(), "article", article)
-//		next.ServeHTTP(w, r.WithContext(ctx))
-//	})
-//}
+func postUpdate(w http.ResponseWriter, r *http.Request) {
+	//This gets/populates the content of the form
+	err := r.ParseForm()
+	handleErr(err)
+
+	for i, _ := range DataStructure {
+		if DataStructure[i].Id == v.Id {
+			DataStructure = append(DataStructure[:i], DataStructure[i+1:]...)
+		}
+	}
+
+	topic := r.PostForm.Get("topic")
+	content := r.PostForm.Get("content")
+
+	v.Topic = topic
+	v.Content = content
+
+	DataStructure = append(DataStructure, v)
+
+	http.Redirect(w, r, "/", 302)
+}
